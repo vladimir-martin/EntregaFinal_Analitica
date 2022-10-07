c82c12a5b76da9b5524ae0299bcece4eb1128b10
vladimir-martin <114034302+vladimir-martin@users.noreply.github.com>
2022-10-06 23:25:20 -0500

    rev. 2

--- final.py
+++ final.py
@@ -6,6 +6,7 @@ import plotly.express as px
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 import base64
+import geojson
 # Configuraciones de la pagina
 st.set_page_config(page_title="Turismo en Bogotá", page_icon="https://www.shd.gov.co/plantillas/images/logo-bogota.png", initial_sidebar_state="expanded", menu_items={"About":"DashBoard sobre la actualidad turistica de Bogotá, generado para al clase de Analitica I, UdeA 2022-1. Grupo: Dayan Marin, Vladimir Martin"},layout="wide")
 
@@ -25,6 +26,17 @@ salaexpo = pd.read_csv('salasexposiciongalerias_4.csv',encoding='latin-1',sep=";
 avion = pd.read_csv("pasajeros.csv",encoding='latin-1',sep=";")
 avion_int = pd.read_csv("pasajeros_int.csv",encoding='latin-1',sep=";")
 comidas = pd.read_csv('comidas_bog.csv',encoding='latin-1',sep=",")
+# importar geojson
+with open('Area Protegida.geojson') as f:
+  gj_com = geojson.load(f)
+#CREACION COLUMNA PARA DISCRIMINAR
+teatro["PLAN"] = "Teatros y Auditorios"
+museos["PLAN"] = "Museos"
+centros["PLAN"] = "Centros Culturales y Artisticos"
+salacine["PLAN"] = "Salas de Cine y Cinematecas"
+salaexpo["PLAN"] = "Salas de Exposiciones y Galerias"
+#se crea base de datos unida
+db_unida = pd.concat([salaexpo, salacine, centros,museos,teatro]) 
 
 #cambiar formato a fecha
 rnt["ANO"]=pd.to_datetime(rnt["ANO"],format='%Y')
@@ -40,6 +52,8 @@ rnt["MES_N"]=rnt['MES'].dt.strftime('%B')
 #se remplazan comas por puntos, como separador decimal
 arpro["SHAPE_AREA"]=arpro["SHAPE_AREA"].str.replace(",",".")
 arpro["SHAPE_LEN"]=arpro["SHAPE_LEN"].str.replace(",",".")
+#se remplaza valor mal escrito
+arpro["TIPO"]=arpro["TIPO"].replace(["Parque Ecologico Distrital de Monta??a"],"Parque Ecologico Distrital de Montaña")
 
 
 #Se eliminan columnas que no aportan al analisis
@@ -62,16 +76,6 @@ teatro['LECNOMLOC']=teatro['LECNOMLOC'].replace(["USAQUÉN"],"USAQUEN")
 
 teatro=teatro.drop(['LECESTADO', "LECCONTACTO"], axis=1)
 
-
-#importar imagen de disco local
-# =============================================================================
-# @st.experimental_memo
-# def get_img_as_base64(file):
-#     with open(file,"rb")as f:
-#         data = f.read()
-#     return base64.b64encode(data).decode()
-# img=get_img_as_base64("image.jpg")
-# =============================================================================
 
 #CSS style tag para apariencias de la pagina, los data-testid se encuentran mediante las devtools del navegador. tutorial: https://www.youtube.com/watch?v=pyWqw5yCNdo
 page_bg_img="""
@@ -114,8 +118,7 @@ with st.sidebar:
 # Título principal
 st.markdown("<h1 style='text-align: center; color: #e4032e;'>🏙️Panorama Turistico de Bogotá🏙️</h1>", unsafe_allow_html=True)
 st.markdown("<h2 style='text-align: center; color: #fab62d;'>🗺️¿Que puedo hacer en la Ciudad?🗺️ </h2>", unsafe_allow_html=True)
-
-
+st.markdown("<h2 style='text-align: center; color: #fab62d;'> </h2>", unsafe_allow_html=True)
 
 ##CREACION DEL DATASET DE REGISTROS TURISTICOS
 rnt_dep=rnt.groupby(["DEPARTAMENTO"])[["CODIGO_RNT"]].count().rename(columns={"CODIGO_RNT":"REGISTROS"}).reset_index().sort_values('REGISTROS',ascending=False)
@@ -137,9 +140,35 @@ with st.sidebar:
     st.markdown("<h2 style= color: #fab62d;'>Entradas Internacionales de pasajeros por ✈️ Año 2021", unsafe_allow_html=True)    
     st.write(avion_int) 
 
-#prestadores de servicios turisticos
+#grafica ubicacion por departamento
+# =============================================================================
+st.markdown("<h2 style='text-align: center; color: #646363;'>🌐Departamentos con mayor numero de registros de empresas de turismo", unsafe_allow_html=True)
+c1,c2=st.columns((4,6))
+#creacion del dataset
+rnt_dep=rnt.groupby(["DEPARTAMENTO"])[["CODIGO_RNT"]].count().rename(columns={"CODIGO_RNT":"REGISTROS"}).reset_index().sort_values('REGISTROS',ascending=False)
+rnt_dep_f=rnt_dep[rnt_dep["REGISTROS"]>20000]
+#grafica
+# crear gráfica:
+rnt_dep_fig = px.pie(rnt_dep_f , values = 'REGISTROS', names = 'DEPARTAMENTO')
 
-st.markdown("<h2 style= color: #fab62d;'>Tipos de negocio registados para turismo en la Ciudad", unsafe_allow_html=True)
+# agregar detalles a la gráfica:
+rnt_dep_fig.update_layout(
+    template = 'ggplot2',
+    legend_title = 'DEPARTAMENTOS',
+    title_x = 0.5,
+    paper_bgcolor='rgba(0,0,0,0)',
+    plot_bgcolor='rgba(0,0,0,0)')
+
+c2.plotly_chart(rnt_dep_fig)
+c1.markdown("<h1 style='text-align: center;'<br> </h1>", unsafe_allow_html= True)
+c1.image("https://pbs.twimg.com/media/ELmHWGsX0AAyRbZ.jpg",width=600)
+
+
+
+#prestadores de servicios turisticos
+# =============================================================================
+st.markdown("<h2 style='text-align: center; color: #646363;'>🖊️Tipos de negocio registados para turismo en la Ciudad", unsafe_allow_html=True)
+c4,c5= st.columns((7,6))
 #se genera el dataframe
 rnt_bog=rnt[rnt["DEPARTAMENTO"].str.contains("BOGOTA")]
 rnt_cat=rnt_bog.groupby(["CATEGORIA"])[["CODIGO_RNT"]].count().reset_index().rename(columns={"CODIGO_RNT":"LUGARES"}).sort_values('LUGARES',ascending=False)
@@ -157,12 +186,126 @@ rnt_cat_fig.update_layout(
     paper_bgcolor='rgba(0,0,0,0)',
     plot_bgcolor='rgba(0,0,0,0)')
 
-st.plotly_chart(rnt_cat_fig)
+c4.plotly_chart(rnt_cat_fig)
 
+
+c5.image("https://bogota.gov.co/sites/default/files/styles/1050px/public/field/image/Recorrido-turistico.jpg",caption="Recorridos Turisticos en el barrio la Candelaria",width=700)
+ 
+
+#reservas naturales de la Ciudad
+# =============================================================================
+st.markdown("<h2 style='text-align: center; color: #646363;'>🏞️Reservas Naturales de la Ciudad", unsafe_allow_html=True)
+c4,c5= st.columns((6,7))
+#creacion del dataframe
+arpro_tipo=arpro.groupby(["TIPO"])[["NOMBRE"]].count().reset_index().rename(columns={"NOMBRE":"CANTIDAD"})
+#Se corrige error en una categoria
+arpro_tipo["TIPO"]=arpro_tipo["TIPO"].replace("Parque Ecologico Distrital de Monta??a","Parque Ecologico Distrital de Montaña")
+#se saca el total de areas protegicas
+total_arpro=arpro["TIPO"].count()
+c5.markdown("<h4 style='text-align: center; color: #646363;'>Porcentajes por Tipo de Reserva", unsafe_allow_html=True)
+# elaborar el grafico
+arpro_tipo_fig = px.pie(arpro_tipo , values = 'CANTIDAD', names = 'TIPO', template = 'ggplot2',
+             hole = .5)
+# poner detalles a la gráfica
+arpro_tipo_fig.update_layout(    
+    title_x = 0.5,
+    paper_bgcolor='rgba(0,0,0,0)',
+    plot_bgcolor='rgba(0,0,0,0)',
+    annotations = [dict(text = str(total_arpro), x=0.5, y = 0.5, font_size = 40, showarrow = False )],
+    legend=dict(orientation="h",
+                yanchor="bottom",
+                y=-0.3,
+                xanchor="left",
+                x=0))
+
+c5.plotly_chart(arpro_tipo_fig)
+c4.image("https://contenidos.civico.com/wp-content/uploads/2014/03/sumapaz-ex_magician.jpg",caption="Páramo Natural de Sumapaz",width=600)
+
+#mapa areas protegidas
+token_map = "pk.eyJ1IjoidmxhZGlnb3NwZWwiLCJhIjoiY2w4YW95aXNmMDdjejN1bWh2bjNkYjNqMyJ9.ScoXk60DLIaoYQq-uXNU3w"
+px.set_mapbox_access_token(token_map)
+#generar grafico
+fig = px.choropleth_mapbox(arpro, # dataframe que tiene el indicador
+              geojson = gj_com, # archivo json con el shape
+              color = 'TIPO', # columna que contiene el indicador: valor sobre el cual se va dar la tonalidad del color
+              locations = 'NOMBRE', # llave del dataframe para hacer el join con el shape
+              featureidkey = 'properties.NOMBRE', # llave del shape para hacer el join con el dataframe
+              hover_name = 'NOMBRE', # información que se va a observar cuando se pase el cursor por el poligono
+              center = {'lat':4.29, 'lon':-74.21}, # centro en el cual se va ubicar el mapa (en este caso centroíde de Medellin)
+              zoom = 7.9, # zoom de la imagen
+              mapbox_style="streets",
+              opacity=0.5)
+fig.update_layout(    
+    title_x = 0.5,
+    paper_bgcolor='rgba(0,0,0,0)',
+    plot_bgcolor='rgba(0,0,0,0)',
+    margin={"r":0,"t":0,"l":0,"b":0},
+    legend=dict(orientation="h",
+                yanchor="bottom",
+                y=-0.1,
+                xanchor="left",
+                x=0))
+      
+fig.update_geos(fitbounds = 'locations', visible = False) # ajustar a los limites del shape
+st.plotly_chart(fig,use_container_width=True)
+
+#planes por localidad
+# =============================================================================
+st.markdown("<h2 style='text-align: center; color: #646363;'>🚶‍♂️Planes en Bogotá por localidad", unsafe_allow_html=True)
+#creacion del dataframe
+db_unida_loc=db_unida.groupby(["LECNOMLOC","PLAN"])[["LECCODIGO"]].count().reset_index().rename(columns={"LECNOMLOC":"LOCALIDAD","LECCODIGO":"CANTIDAD"}).sort_values(by='CANTIDAD', ascending=False)
+# se crea la grafica
+db_unida_loc_fig = px.bar(db_unida_loc, x='LOCALIDAD', y='CANTIDAD',color="PLAN",template = 'plotly_dark')
+db_unida_loc_fig.update_layout(
+    xaxis_title = '<b>Localidad<b>',
+    yaxis_title = "<b># de Planes<b>",
+    paper_bgcolor='rgba(0,0,0,0)',
+    plot_bgcolor='rgba(0,0,0,0)',
+    title_x = 0.2,
+    font_size = 16)
+st.plotly_chart(db_unida_loc_fig,use_container_width=True)
+
+#Galerias por zona
+# =============================================================================
+st.markdown("<h2 style='text-align: center; color: #646363;'>🖼️Galerias de Arte y Salas de exposiciones", unsafe_allow_html=True)
+c1,c2=st.columns((6,4))
+#creando el dataframe
+salaexpo_gen=salaexpo.groupby(["LECNOMUPZ","LECCLASIF"])[["LECCODIGO"]].count().reset_index().rename(columns={"LECCLASIF":"TIPO","LECNOMUPZ":"ZONA-UPZ","LECCODIGO":"CANTIDAD"}).sort_values('CANTIDAD',ascending=False)
+salaexpo_gen_fig = px.bar(salaexpo_gen, x='ZONA-UPZ', y='CANTIDAD',template = 'plotly_dark',color="TIPO",barmode="stack")
+salaexpo_gen_fig.update_layout(
+    xaxis_title = '<b>Zona-UPZ<b>',
+    yaxis_title = '<b>Cantidad<b>',
+    paper_bgcolor='rgba(0,0,0,0)',
+    plot_bgcolor='rgba(0,0,0,0)',
+    title_x = 0.2,
+    legend=dict(orientation="h",
+                yanchor="top",
+                y=1.1,
+                xanchor="left",
+                x=0))
+c1.plotly_chart(salaexpo_gen_fig,use_container_width=True)
+c2.image("https://cloudfront-us-east-1.images.arcpublishing.com/semana/SDYMMOXJJZEORETXTBZOJ5AKSE.jpg",caption="Galeria BETA",width=650)
+#Cines por Marca
+# =============================================================================
+st.markdown("<h2 style='text-align: center; color: #646363;'>🎬Cines y Cinematecas", unsafe_allow_html=True)
+c1,c2=st.columns((4,6))
+#Creacion del dataframe
+salacine_tot=salacine.groupby(["LECCLASIF"])[["LECCODIGO"]].count().reset_index().rename(columns={"LECCODIGO":"CANTIDAD","LECCLASIF":"MARCA"}).sort_values('CANTIDAD',ascending=False)
+# se crea la grafica
+salacine_tot_fig = px.bar(salacine_tot, x='MARCA', y='CANTIDAD',template = 'plotly_dark')
+salacine_tot_fig.update_layout(
+    xaxis_title = '<b>Marca<b>',
+    yaxis_title = '<b>Cantidad<b>',
+    paper_bgcolor='rgba(0,0,0,0)',
+    plot_bgcolor='rgba(0,0,0,0)',
+    title_x = 0.2)
+c2.plotly_chart(salacine_tot_fig,use_container_width=True)
+c1.image("https://www.eltiempo.com/files/image_640_428/uploads/2019/06/11/5d0031291de28.jpeg",caption="Cinemateca de Bogotá",width=500)
 
 #mapa de comidas
+# =============================================================================
 #se agrega nueva base de datos de comida
-st.markdown("<h2 style= color: #fab62d;'>🍝Ubicacion de sitios de comida en la Ciudad", unsafe_allow_html=True)
+st.markdown("<h2 style= 'text-align: center; color: #646363;'>🍝Ubicacion de sitios de comida en la Ciudad", unsafe_allow_html=True)
 comidas=comidas.rename(columns={"amenity":"Tipo","name":"Nombre","cuisine":"Especialidad"})
 comidas["Especialidad"]=comidas["Especialidad"].replace({'friture':"Comidas Rapidas", 'coffee_shop':"Cafe", 'regional':"Regional", 'burger':"Comidas Rapidas", 'heladeria':"Helados",
        'bread':"Panaderia", 'ice_cream':"Helados", 'panaderia':"Panaderia", 'chinese':"Comida China",
@@ -184,17 +327,22 @@ mapa_1=px.scatter_mapbox(comidas,
                   mapbox_style  = 'streets',
                   color_continuous_scale = px.colors.cyclical.IceFire, size_max = 30, zoom = 10,
                   template = 'simple_white',
-                  width=1250,
+                  width=1200,
                   height=600)
 mapa_1.update_layout(
     paper_bgcolor='rgba(0,0,0,0)',
     plot_bgcolor='rgba(0,0,0,0)',
-    legend_borderwidth=0.9,
-    legend_font_size=16)
+    legend_font_size=16,
+    font_size = 50,
+    legend=dict(orientation="h",
+                yanchor="bottom",
+                y=-0.1,
+                xanchor="left",
+                x=0))
 
-st.plotly_chart(mapa_1) 
+st.plotly_chart(mapa_1,use_container_width=True) 
 
-st.map(comidas) # Generar mapa
+
 
 
 
@@ -206,7 +354,15 @@ st.map(comidas) # Generar mapa
 # st.image(image, caption='Bogotá')
 # =============================================================================
 
-
+#importar imagen de disco local
+# =============================================================================
+# @st.experimental_memo
+# def get_img_as_base64(file):
+#     with open(file,"rb")as f:
+#         data = f.read()
+#     return base64.b64encode(data).decode()
+# img=get_img_as_base64("image.jpg")
+# =============================================================================
 
 
 
